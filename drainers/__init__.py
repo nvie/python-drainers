f8198837930b11794e84c6f8391db3e5fae18c4f
Vincent Driessen <vincent@datafox.nl>
2010-05-19 12:11:27 +0200

    Fix URL.

--- drainers/__init__.py
+++ drainers/__init__.py
@@ -7,7 +7,7 @@ VERSION = (0, 0, 1)
 __version__ = ".".join(map(str, VERSION[0:3])) + "".join(VERSION[3:])
 __author__ = "Vincent Driessen"
 __contact__ = "vincent@datafox.nl"
-__homepage__ = "http://github.com/nvie/drainers/"
+__homepage__ = "http://github.com/nvie/python-drainers/"
 #__docformat__ = "restructuredtext"
 
 from drainer import Drainer
